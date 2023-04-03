df66fc7ff80afbe15f6112c579d9d03cd5be0bff
Paul Montesano <paul.montesano@gmail.com>
2023-04-03 09:46:06 -0400

    intersect option for atl08io

--- atl08lib.py
+++ atl08lib.py
@@ -7,7 +7,7 @@ import glob
 import os
 import time
 
-def atl08_io(ATL08_CSV_OUTPUT_DIR, YEAR_SEARCH, DO_PICKLE=True, LENGTH_SEG=100):
+def atl08_io(ATL08_CSV_OUTPUT_DIR, YEAR_SEARCH, DO_PICKLE=True, LENGTH_SEG=100, INTERSECT=None, BUF_DIST=0):
     
     '''Read all ATL08 from CSVs of a given year after extract_filter_atl08.py
         Write to a pickle file by year
@@ -26,13 +26,24 @@ def atl08_io(ATL08_CSV_OUTPUT_DIR, YEAR_SEARCH, DO_PICKLE=True, LENGTH_SEG=100):
     else:  
         # Merge all files in the list
         print("Creating pandas data frame...")
-        atl08_gdf = pd.concat((pd.read_csv(f) for f in all_atl08_csvs ), sort=False, ignore_index=True) # <--generator is (), list is []
+        if INTERSECT is None:
+            atl08_gdf = pd.concat((pd.read_csv(f) for f in all_atl08_csvs ), sort=False, ignore_index=True) # <--generator is (), list is []
+        else:
+            if isinstance(INTERSECT, pd.DataFrame):
+                pass
+            else:
+                INTERSECT = gpd.read_file(INTERSECT)
+                
+            # Intersect each file with INTERSECT gdf and concat    
+            atl08_gdf = pd.concat( ( gpd.overlay(get_gdf_atl08(pd.read_csv(f), LENGTH_SEG), INTERSECT.to_crs(4326), how='intersection') for f in all_atl08_csvs ), sort=False, ignore_index=True)
+        
         if LENGTH_SEG == 20:
-            print(f"Creating a gdf for {YEAR_SEARCH} @ {LENGTH_SEG}m...")
+            
             atl08_gdf.rename(columns={'lon': 'lon_100m', 'lat': 'lat_100m'}, inplace=True)
             atl08_gdf.rename(columns={'lon_20m': 'lon', 'lat_20m': 'lat'}, inplace=True)
             atl08_gdf = atl08_gdf[atl08_gdf.h_can_20m < 3.402823 * 1e38]
-
+            
+        print(f"Creating a gdf for {YEAR_SEARCH} @ {LENGTH_SEG}m...")
         atl08_gdf = GeoDataFrame(atl08_gdf, geometry=gpd.points_from_xy(atl08_gdf.lon, atl08_gdf.lat), crs='epsg:4326')#.sample(frac=SAMP_FRAC)
 
         if DO_PICKLE:
@@ -44,3 +55,13 @@ def atl08_io(ATL08_CSV_OUTPUT_DIR, YEAR_SEARCH, DO_PICKLE=True, LENGTH_SEG=100):
             atl08_gdf.to_pickle(out_pickle_fn)
 
     return(atl08_gdf)
+
+def get_gdf_atl08(atl08_df, LENGTH_SEG):
+    
+    if LENGTH_SEG == 20:
+        
+        atl08_df.rename(columns={'lon': 'lon_100m', 'lat': 'lat_100m'}, inplace=True)
+        atl08_df.rename(columns={'lon_20m': 'lon', 'lat_20m': 'lat'}, inplace=True)
+        atl08_df = atl08_df[atl08_df.h_can_20m < 3.402823 * 1e38]
+        
+    return(GeoDataFrame(atl08_df, geometry=gpd.points_from_xy(atl08_df.lon, atl08_df.lat), crs='epsg:4326'))
