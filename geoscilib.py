fef1f6517129a9d66fabf04a42d0204caea197b4
Paul Montesano <paul.montesano@gmail.com>
2023-10-11 13:29:31 -0400

    nb updates

--- geoscilib.py
+++ geoscilib.py
@@ -13,6 +13,25 @@ import multiprocessing as mp
 from scipy.spatial import cKDTree
 from shapely.geometry import Point
 
+import datetime
+import json
+import warnings
+
+import certifi
+import urllib3
+from urllib.parse import urlencode
+
+from pprint import pprint
+
+COLLECTIONCONCEPTID_DICT = {
+                        'ATL08.003': "C2003772626-NSIDC_ECS",
+                        'ATL08.005': "C2144424132-NSIDC_ECS",
+                        'ATL08.006': "C2565090645-NSIDC_ECS",
+                        'GLIHT': "C2013348111-LPDAAC_ECS",
+                        # our ICESat-2 derived AGBD map c2020
+                        'BOREALAGB2020': "C2756302505-ORNL_CLOUD" 
+}
+
 gpd1 = gpd.GeoDataFrame([['John', 1, Point(1, 1)], ['Smith', 1, Point(2, 2)],
                          ['Soap', 1, Point(0, 2)]],
                         columns=['Name', 'ID', 'geometry'])
@@ -276,3 +295,185 @@ def extract_zonal_gdf(r_fn, GDF, bandnames: list, reproject=True, return_src_pat
         return(GDF)
     else:
         GDF.to_file(os.path.join(OUTDIR, f'zonal_{OUTNAME}_{os.path.splitext(os.path.basename(r_fn))[0]}.gpkg'), driver='GPKG')
+        
+# -----------------------------------------------------------------------------
+# class CmrProcess
+#
+# @author: Caleb Spradlin, caleb.s.spradlin@nasa.gov
+# @version: 12.30.2021
+#
+# https://cmr.earthdata.nasa.gov/search/
+# https://cmr.earthdata.nasa.gov/search/site/docs/search/api.html
+# -----------------------------------------------------------------------------
+class CmrProcess(object):
+
+    CMR_BASE_URL = 'https://cmr.earthdata.nasa.gov' +\
+        '/search/granules.umm_json_v1_4?'
+
+    # Range for valid lon/lat
+    LATITUDE_RANGE = (-90, 90)
+    LONGITUDE_RANGE = (-180, 180)
+
+    # -------------------------------------------------------------------------
+    # __init__
+    # -------------------------------------------------------------------------
+    def __init__(self,
+                 mission,
+                 dateTime,
+                 lonLat=None,
+                 error=False,
+                 dayNightFlag='',
+                 pageSize=150,
+                 maxPages=50):
+
+        self._error = error
+        self._dateTime = dateTime
+        self._mission = mission
+        self._pageSize = pageSize
+        self._maxPages = maxPages
+        
+        self._lonLat = lonLat
+        self._dayNightFlag = dayNightFlag
+
+    # -------------------------------------------------------------------------
+    # run()
+    #
+    # Given a set of parameters on init (time, location, mission), search for
+    # the most relevant file. This uses CMR to search metadata for
+    # relevant matches.
+    # -------------------------------------------------------------------------
+    def run(self):
+        print('Starting query')
+        outout = set()
+        for i in range(self._maxPages):
+            
+            d, e = self._cmrQuery(pageNum=i+1)
+            
+            if e and i > 1:
+                return sorted(list(outout))
+            
+            if not e:
+                print('Results found on page: {}'.format(i+1))
+                out = [r['file_url'] for r in d.values()]
+                outout.update(out)
+                
+        outout = sorted(list(outout))
+        return outout
+        
+
+    # -------------------------------------------------------------------------
+    # cmrQuery()
+    #
+    # Search the Common Metadata Repository(CMR) for a file that
+    # is a temporal and spatial match.
+    # -------------------------------------------------------------------------
+    def _cmrQuery(self, pageNum=1):
+
+        requestDictionary = self._buildRequest(pageNum=pageNum)
+        totalHits, resultDictionary = self._sendRequest(requestDictionary)
+
+        if self._error:
+            return None, self._error
+
+        if totalHits <= 0:
+            print('No hits on page number: {}, ending search.'.format(pageNum))
+            #warnings.warn(msg)
+            return None, True
+
+        resultDictionaryProcessed = self._processRequest(resultDictionary)
+        return resultDictionaryProcessed, self._error
+
+    # -------------------------------------------------------------------------
+    # buildRequest()
+    #
+    # Build a dictionary based off of parameters given on init.
+    # This dictionary will be used to encode the http request to search
+    # CMR.
+    # -------------------------------------------------------------------------
+    def _buildRequest(self, pageNum=1):
+        requestDict = dict()
+        requestDict['page_num'] = pageNum
+        requestDict['page_size'] = self._pageSize
+        requestDict['concept_id'] = self._mission
+        requestDict['bounding_box'] = self._lonLat
+        requestDict['day_night_flag'] = self._dayNightFlag
+        requestDict['temporal'] = self._dateTime
+        return requestDict
+
+    # -------------------------------------------------------------------------
+    # _sendRequest
+    #
+    # Send an http request to the CMR server.
+    # Decode data and count number of hits from request.
+    # -------------------------------------------------------------------------
+    def _sendRequest(self, requestDictionary):
+        with urllib3.PoolManager(cert_reqs='CERT_REQUIRED',
+                                 ca_certs=certifi.where()) as httpPoolManager:
+            encodedParameters = urlencode(requestDictionary, doseq=True)
+            requestUrl = self.CMR_BASE_URL + encodedParameters
+            try:
+                requestResultPackage = httpPoolManager.request('GET',
+                                                               requestUrl)
+            except urllib3.exceptions.MaxRetryError:
+                self._error = True
+                return 0, None
+
+            requestResultData = json.loads(
+                requestResultPackage.data.decode('utf-8'))
+            status = int(requestResultPackage.status)
+
+            if not status == 400:
+                totalHits = len(requestResultData['items'])
+                return totalHits, requestResultData
+
+            else:
+                msg = 'CMR Query: Client or server error: ' + \
+                    'Status: {}, Request URL: {}, Params: {}'.format(
+                        str(status), requestUrl, encodedParameters)
+                warnings.warn(msg)
+                return 0, None
+
+    # -------------------------------------------------------------------------
+    # _processRequest
+    #
+    # For each result in the CMR query, unpackage relevant information to
+    # a dictionary. While doing so set flags if data is not desirable (too
+    # close to edge of dataset).
+    #
+    #  REVIEW: Make the hard-coded names class constants? There are a lot...
+    # -------------------------------------------------------------------------
+    def _processRequest(self, resultDict):
+
+        resultDictProcessed = dict()
+
+        for hit in resultDict['items']:
+
+            fileName = hit['umm']['RelatedUrls'][0]['URL'].split(
+                '/')[-1]
+
+            # ---
+            # These are hardcoded here because the only time these names will
+            # ever change is if we changed which format of metadata we wanted
+            # the CMR results back in.
+            #
+            # These could be placed as class constants in the future.
+            # ---
+            fileUrl = hit['umm']['RelatedUrls'][0]['URL']
+            temporalRange = hit['umm']['TemporalExtent']['RangeDateTime']
+            dayNight = hit['umm']['DataGranule']['DayNightFlag']
+
+ 
+            spatialExtent = hit['umm']['SpatialExten' +
+                                          't']['HorizontalSpatialDom' +
+                                               'ain']
+
+            key = fileName
+
+            resultDictProcessed[key] = {
+                'file_name': fileName,
+                'file_url': fileUrl,
+                'temporal_range': temporalRange,
+                'spatial_extent': spatialExtent,
+                'day_night_flag': dayNight}
+
+        return resultDictProcessed
